8f3464fdd1a43f38ecab3d98cabb91b3f03391f1
alpha <kvnswny@gmail.com>
2015-01-19 20:41:50 -0500

    packing list move to Packing_list class

--- trip_planner_test.rb
+++ trip_planner_test.rb
@@ -1,6 +1,8 @@
 require 'httparty'
 require 'cgi'
 
+
+
 class TripPlanner
 	attr_reader :user, :forecast, :recommendation
 
@@ -15,16 +17,16 @@ class TripPlanner
 	end
 
 	def create_user
-		# puts("What is your name?")
-		# @name = gets().chomp()
-		# puts("Which city?")
-		# @city = CGI.escape(gets().chomp())
-		# puts("How many days?")
-		# @days = gets().chomp().to_i
-		@name = "Sasha Banks"
-		@city = CGI.escape("Nome, AK")
-		@days = 3
-		@user = User.new(@name, @city, @days)
+		puts("What is your name?")
+		@name = gets().chomp()
+		puts("Which city?")
+		@city = CGI.escape(gets().chomp())
+		puts("How many days?")
+		@days = gets().chomp().to_i
+		# @name = "Nameford G. Namington"
+		# @city = CGI.escape("Miami, FL")
+		# @days = 3
+		# @user = User.new(@name, @city, @days)
 	end
 
 	def get_forecast
@@ -36,7 +38,7 @@ class TripPlanner
 	def show_forecast
 		@trip_min = 999
 		@trip_max = -999
-		conditions = Array.new()
+		@conditions = Array.new()
 		puts("#{@name}, here is your forecast:")
 		i = 0
 		while i < @days
@@ -46,36 +48,71 @@ class TripPlanner
 			@trip_min = min if min < @trip_min
 			max = (@forecasts['list'][i]['temp']['max']).round
 			@trip_max = max if max > @trip_max
-			conditions.push(@condition)
+			@conditions.push(@condition)
 			puts("On #{date}, #{@city} will have #{@condition}, high of #{max} and low of #{min}.")
 			i += 1
 		end
-		conditions = conditions.uniq
+		@conditions = @conditions.uniq
 	end
 
 	def packing_list
+		packing_list = Packing_list.new(@trip_max, @trip_min, @conditions)
+		packing_list.pack
+	end
+end
+
+
+
+class Packing_list
+	attr_reader :trip_max, :trip_min, :conditions
+
+	def initialize(trip_max, trip_min, conditions)
+		@trip_max = trip_max
+		@trip_min = trip_min
+		@conditions = conditions
+	end
+
+	def pack
 		packing_list = Array.new
 		j = 0
 		while j < CLOTHES.length
-			packing_list << CLOTHES[j][0][:bring]
+			if ( (@trip_min > CLOTHES[j][0][:min_temp]) && (@trip_min < CLOTHES[j][0][:max_temp]) )
+				packing_list << CLOTHES[j][0][:bring]
+			end
+			if ( (@trip_max < CLOTHES[j][0][:max_temp]) && (@trip_max > CLOTHES[j][0][:min_temp]) )
+				packing_list << CLOTHES[j][0][:bring]
+			end
 			j += 1
 		end
-		puts packing_list
+		packing_list = (packing_list.uniq)
+		puts CLOTHES.length
+		puts "The temperature will range from #{@trip_min} to #{trip_max}.  You should bring #{packing_list}"
 	end
 
 	CLOTHES =
 	[
 		{min_temp: -50, max_temp: 0, bring: [
-			"parka", "fleece", "gloves", "scarf"]
+			"heavy coat", "face mask", "mittens", "fleece-lined jeans"]
 		}
 	],
 	[
-		{min_temp: 75, max_temp: 110, bring: [
-				"shorts", "flip-flops", "sunglasses"]
+		{min_temp: 0, max_temp: 32, bring: [
+			"gloves", "coat", "sweatshirt"]
+		}
+	],
+	[
+		{min_temp: 70, max_temp: 100, bring: [
+			"shorts", "flip-flops", "sunglasses"]
+		}
+	],
+	[
+		{min_temp: 100, max_temp: 999, bring: [
+			"that suit that Mr. Freeze has"]
 		}
 	]
-
 end
+
+
 
 class User
 	attr_reader :name, :city, :days
@@ -83,6 +120,8 @@ class User
 	def initialize(name, city, days)
 	end
 end
+
+
 
 trip_plan = TripPlanner.new
 trip_plan.plan
